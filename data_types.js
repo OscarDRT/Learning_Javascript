c683dabf90b0106022e873dce4fc79b5f340d0d9
oscar <oscarnetworkingpro@gmail.com>
2020-03-20 17:08:31 -0500

    First lesson

---
+++ data_types.js
@@ -0,0 +1,49 @@
+// data types
+// Primitives
+let name = 'Oscar'; //String
+console.log(name);
+console.log(typeof name);
+
+let age = 26; //Number
+console.log(age);
+console.log(typeof age);
+
+let size = 1.80; // Number
+console.log(size)
+console.log(typeof size)
+
+let flag = true; // or false Boolean
+console.log(flag);
+console.log(typeof flag);
+
+let special = null; // Object
+console.log(special);
+console.log(typeof special);
+
+let special2; //Undefined
+console.log(special2);
+console.log(typeof special2);
+
+let valor = Symbol('value'); //Symbol
+console.log(valor);
+console.log(typeof valor);
+
+//Objects
+let array1 = [1,2,3]; //Object
+console.log(array1);
+console.log(typeof array1);
+
+let array2 = new Array(4,5,6); //Object
+console.log(array2);
+console.log( typeof array2)
+
+let obj = {      //Object
+  name: 'Oscar',
+  age: 26
+};
+console.log(obj);
+console.log(typeof obj);
+
+let value = new Date(); //Object
+console.log(value);
+console.log(typeof value);
