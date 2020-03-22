b8e5e3794a2e000d06287ffe4b842ae4917a1b54
oscar <oscarnetworkingpro@gmail.com>
2020-03-21 20:39:34 -0500

    15 lesson

---
+++ window_object.js
@@ -0,0 +1,28 @@
+let high,
+    width;
+
+high = window.outerHeight;
+width = window.outerWidth;
+console.log(high);
+console.log(width);
+
+high = window.innerHeight;
+width = window.innerWidth;
+console.log(high);
+console.log(width);
+
+let local;
+local = window.location;
+console.log(local); //URL
+
+local = window.navigator;
+console.log(local);
+
+ocal = window.navigator.appName;
+console.log(local);
+
+ocal = window.navigator.appName;
+console.log(local);
+
+ocal = window.navigator.language;
+console.log(local);
